0e7837848848b8730e6692e4b797c7fd6182b7ab
Max <maxim79135@gmail.com>
2020-10-08 17:15:16 +0300

    Add imgs for task2 lab2 os

--- OS/Labs/Lab2/Task2/main.cpp
+++ OS/Labs/Lab2/Task2/main.cpp
@@ -1,11 +1,17 @@
-#include "widget.h"
+#include <cstdlib>
+#include <ctime>
 
 #include <QApplication>
 
-int main(int argc, char *argv[])
-{
-    QApplication a(argc, argv);
-    Widget w;
-    w.show();
-    return a.exec();
+#include "widget.h"
+
+int main(int argc, char *argv[]) {
+  //    srand(time(nullptr));
+  srand(0);
+
+  QApplication a(argc, argv);
+  Widget w;
+  w.show();
+
+  return a.exec();
 }
