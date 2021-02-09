c34835d68385277a0918d9de157eb69b5869b971
root <root@v33009.hosting-minecraft.ru>
2021-02-09 08:20:38 +0300

    small fixes

--- Data protection/Labs/Lab1/client.py
+++ Data protection/Labs/Lab1/client.py
@@ -1,13 +1,13 @@
 import requests
 import pcid
-from PyQt5.QtWidgets import *
+#from PyQt5.QtWidgets import *
 
 SERVER_URL = 'http://135.181.233.195:5000'
 
 id_ = pcid.pcid()
-app = QApplication([])
-label = QLabel()
-app.exec_()
+#app = QApplication([])
+#label = QLabel('')
+#app.exec_()
 
 
 try:
@@ -16,11 +16,11 @@ try:
     }).text
     
     if response == 'TRUE':
-        label.setText("Программа активирована")
+        #label.setText("Программа активирована")
         print('Program is activated')
     else:
-        label.setText("Нет доступа")
+        #label.setText("Нет доступа")
         print('Program is not activated')
-    label.show()
+    #label.show()
 except:
     print('Can not check activation status')
