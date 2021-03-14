d8264ed8991cb713cbfdaf9a8102cb87ae2452fd
Max <maxim79135@gmail.com>
2021-03-14 12:24:03 +0300

    Task1 in data protection lab3

---
+++ Data protection/Labs/Lab3/KotlinLab3/src/main/kotlin/Task3.kt
@@ -0,0 +1,56 @@
+import java.math.BigInteger
+import kotlin.math.sqrt
+
+class Task3 {
+    private val p = 431
+    private val q = 307
+    private var n = 0
+    private var e = 0
+    private var d = 0
+
+    private fun isPrime(num: Int): Boolean {
+        if (num <= 1) return false
+
+        for (i in 2..sqrt(num.toDouble()).toInt()) {
+            if (num % i == 0) return false
+        }
+        return true
+    }
+
+    fun createKeys() {
+        n = p * q
+        val f = (p - 1) * (q - 1)
+
+        var eArray = emptyArray<Int>()
+        for (i in 1..f) {
+            if (isPrime(i) and (f % i != 0)) eArray += i
+        }
+
+        e = eArray.random()
+        d = 1
+        var bigD: BigInteger = d.toBigInteger()
+        while (bigD.multiply(e.toBigInteger()).mod(f.toBigInteger()).toInt() != 1) {
+            d += 1
+            bigD = d.toBigInteger()
+        }
+
+    }
+
+fun encrypt(message: String): Array<Int> {
+    var ret = emptyArray<Int>()
+    for (c in message) {
+        val tmp: BigInteger = c.toInt().toBigInteger()
+        ret += tmp.modPow(e.toBigInteger(), n.toBigInteger()).toInt()
+    }
+    return ret
+}
+
+fun decrypt(message: Array<Int>): String {
+    var ret = ""
+    for (c in message) {
+        val tmp: BigInteger = c.toBigInteger()
+        ret += tmp.modPow(d.toBigInteger(), n.toBigInteger()).toInt().toChar()
+    }
+    return ret
+}
+}
--- Data protection/Labs/Lab3/KotlinLab3/src/main/kotlin/main.kt
+++ Data protection/Labs/Lab3/KotlinLab3/src/main/kotlin/main.kt
@@ -1,3 +1,34 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import javafx.concurrent.Task
+
+fun main() {
+//    val task3 = Task3()
+//    var str: String
+//
+//    var array = emptyArray<Int>()
+//
+//    while (true) {
+//        str = readLine()!!
+//        when (str) {
+//            "create" -> task3.createKeys()
+//            "encrypt" -> {
+//                array = task3.encrypt("Hello")
+//                println(array.toList())
+//            }
+//            "decrypt" -> {
+//                println(task3.decrypt(array))
+//            }
+//            "exit" -> return
+//        }
+//    }
+    testTask3()
+
+}
+
+fun testTask3() {
+    val task3 = Task3()
+
+    task3.createKeys()
+    val array = task3.encrypt("Max Sedov")
+    println(array.toList())
+    println(task3.decrypt(array))
 }
